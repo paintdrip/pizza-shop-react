885fb9f519d83971fb13e7b4e08c18c8e1512bca
paintdrip <pa1nkller8879@gmail.com>
2022-10-19 21:15:25 +0300

    first
    transferring html markup to react components, connecting sass

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+
+// Основной файл проекта, хранящий данные передаваемые в root
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Мексиканская" price={"500"} />
+            <PizzaBlock title="Мексиканская" price={"500"} />
+            <PizzaBlock title="Мексиканская" price={"500"} />
+            <PizzaBlock title="Мексиканская" price={"500"} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
