07647b0834d71b51d3ce10ce704c3247cb269a03
paintdrip <pa1nkller8879@gmail.com>
2022-11-16 23:50:16 +0300

    pagination

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -10,7 +10,6 @@ const Search = () => {
     <div className={styles.root}>
       <svg
         className={styles.icon}
-        enable-background="new 0 0 32 32"
         id="Glyph"
         version="1.1"
         viewBox="0 0 32 32"
@@ -38,10 +37,8 @@ const Search = () => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            clip-rule="evenodd"
             d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM4.29289 5.70711L6.58579 8L4.29289 10.2929L5.70711 11.7071L8 9.41421L10.2929 11.7071L11.7071 10.2929L9.41421 8L11.7071 5.70711L10.2929 4.29289L8 6.58579L5.70711 4.29289L4.29289 5.70711Z"
             fill="#030708"
-            fill-rule="evenodd"
           />
         </svg>
       )}
