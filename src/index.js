2c0fa3645e932a174b6ee162bcd600579748e391
paintdrip <pa1nkller8879@gmail.com>
2022-11-19 17:48:08 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import App from "./App";
-
 import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
