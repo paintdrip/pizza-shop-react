1963688c5b2199b27ac2a730b333a48ccd149e9c
Anton Kulbitskiy <pa1nkller8879@gmail.com>
2022-12-02 18:23:02 +0300

    useRef on input

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,48 +1,55 @@
-import React from "react";
-import styles from "./Search.module.scss";
-import { SearchContext } from "../../App";
+import React from 'react';
+import styles from './Search.module.scss';
+import { SearchContext } from '../../App';
 
 const Search = () => {
-  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+	const { searchValue, setSearchValue } = React.useContext(SearchContext);
+	const inputRef = React.useRef();
 
-  return (
-    <div className={styles.root}>
-      <svg
-        className={styles.icon}
-        id="Glyph"
-        version="1.1"
-        viewBox="0 0 32 32"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M27.414,24.586l-5.077-5.077C23.386,17.928,24,16.035,24,14c0-5.514-4.486-10-10-10S4,8.486,4,14  s4.486,10,10,10c2.035,0,3.928-0.614,5.509-1.663l5.077,5.077c0.78,0.781,2.048,0.781,2.828,0  C28.195,26.633,28.195,25.367,27.414,24.586z M7,14c0-3.86,3.14-7,7-7s7,3.14,7,7s-3.14,7-7,7S7,17.86,7,14z"
-          id="XMLID_223_"
-        />
-      </svg>
-      <input
-        value={searchValue}
-        onChange={(event) => setSearchValue(event.target.value)}
-        className={styles.input}
-        placeholder="Поиск пиццы..."
-      />
-      {searchValue && (
-        <svg
-          onClick={() => setSearchValue("")}
-          className={styles.clear}
-          fill="none"
-          height="16"
-          viewBox="0 0 16 16"
-          width="16"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM4.29289 5.70711L6.58579 8L4.29289 10.2929L5.70711 11.7071L8 9.41421L10.2929 11.7071L11.7071 10.2929L9.41421 8L11.7071 5.70711L10.2929 4.29289L8 6.58579L5.70711 4.29289L4.29289 5.70711Z"
-            fill="#030708"
-          />
-        </svg>
-      )}
-    </div>
-  );
+	const onClickClear = () => {
+		setSearchValue('');
+		inputRef.current.focus();
+	};
+
+	return (
+		<div className={styles.root}>
+			<svg
+				className={styles.icon}
+				id='Glyph'
+				version='1.1'
+				viewBox='0 0 32 32'
+				xmlns='http://www.w3.org/2000/svg'
+			>
+				<path
+					d='M27.414,24.586l-5.077-5.077C23.386,17.928,24,16.035,24,14c0-5.514-4.486-10-10-10S4,8.486,4,14  s4.486,10,10,10c2.035,0,3.928-0.614,5.509-1.663l5.077,5.077c0.78,0.781,2.048,0.781,2.828,0  C28.195,26.633,28.195,25.367,27.414,24.586z M7,14c0-3.86,3.14-7,7-7s7,3.14,7,7s-3.14,7-7,7S7,17.86,7,14z'
+					id='XMLID_223_'
+				/>
+			</svg>
+			<input
+				ref={inputRef}
+				value={searchValue}
+				onChange={(event) => setSearchValue(event.target.value)}
+				className={styles.input}
+				placeholder='Поиск пиццы...'
+			/>
+			{searchValue && (
+				<svg
+					onClick={onClickClear}
+					className={styles.clear}
+					fill='none'
+					height='16'
+					viewBox='0 0 16 16'
+					width='16'
+					xmlns='http://www.w3.org/2000/svg'
+				>
+					<path
+						d='M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM4.29289 5.70711L6.58579 8L4.29289 10.2929L5.70711 11.7071L8 9.41421L10.2929 11.7071L11.7071 10.2929L9.41421 8L11.7071 5.70711L10.2929 4.29289L8 6.58579L5.70711 4.29289L4.29289 5.70711Z'
+						fill='#030708'
+					/>
+				</svg>
+			)}
+		</div>
+	);
 };
 
 export default Search;
