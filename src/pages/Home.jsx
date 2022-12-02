2be35075febe9a9516c7fa5b83ee0c8863ef0b51
Anton Kulbitskiy <pa1nkller8879@gmail.com>
2022-12-02 18:00:24 +0300

    fetch => axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,65 +1,63 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from 'react';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { setCategoryId } from "../redux/slices/filterSlice";
-import Categories from "../components/Categories";
-import Sort from "../components/Sort";
-import PizzaSkeleton from "../components/Skeleton";
-import PizzaBlock from "../components/PizzaBlock";
-import Pagination from "../components/Pagination/Pagination";
-import { SearchContext } from "../App";
+import { setCategoryId } from '../redux/slices/filterSlice';
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaSkeleton from '../components/Skeleton';
+import PizzaBlock from '../components/PizzaBlock';
+import Pagination from '../components/Pagination/Pagination';
+import { SearchContext } from '../App';
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const { categoryId, sort } = useSelector((state) => state.filter);
+	const dispatch = useDispatch();
+	const { categoryId, sort } = useSelector((state) => state.filter);
 
-  console.log("id category", categoryId);
+	const { searchValue } = React.useContext(SearchContext);
+	const [items, setItems] = React.useState([]);
+	const [isLoading, setIsLoading] = React.useState(true);
+	const [currentPage, setCurrentPage] = React.useState(1);
 
-  const { searchValue } = React.useContext(SearchContext);
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [currentPage, setCurrentPage] = React.useState(1);
+	const onChangeCategory = (id) => {
+		dispatch(setCategoryId(id));
+	};
 
-  const onChangeCategory = (id) => {
-    dispatch(setCategoryId(id));
-  };
+	React.useEffect(() => {
+		setIsLoading(true);
 
-  React.useEffect(() => {
-    setIsLoading(true);
+		const sortBy = sort.sortProperty.replace('-', '');
+		const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
+		const category = categoryId > 0 ? `category=${categoryId}` : '';
+		const search = searchValue ? `&search=${searchValue}` : '';
 
-    const sortBy = sort.sortProperty.replace("-", "");
-    const order = sort.sortProperty.includes("-") ? "asc" : "desc";
-    const category = categoryId > 0 ? `category=${categoryId}` : "";
-    const search = searchValue ? `&search=${searchValue}` : "";
+		axios
+			.get(
+				`https://635fe9e43e8f65f283bed842.mockapi.io/pizzas?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+			)
+			.then((res) => {
+				setItems(res.data);
+				setIsLoading(false);
+			});
 
-    fetch(
-      `https://635fe9e43e8f65f283bed842.mockapi.io/pizzas?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
-    )
-      .then((res) => res.json())
-      .then((array) => {
-        setItems(array);
-        setIsLoading(false);
-      });
-    window.scrollTo(0, 0);
-  }, [categoryId, sort.sortProperty, searchValue, currentPage]);
+		window.scrollTo(0, 0);
+	}, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
-  const dishes = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+	const dishes = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
 
-  const skeletons = [...new Array(6)].map((_, index) => (
-    <PizzaSkeleton key={index} />
-  ));
+	const skeletons = [...new Array(6)].map((_, index) => <PizzaSkeleton key={index} />);
 
-  return (
-    <div className="container">
-      <div className="content__top">
-        <Categories value={categoryId} onChangeCategory={onChangeCategory} />
-        <Sort />
-      </div>
-      <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">{isLoading ? skeletons : dishes}</div>
-      <Pagination onChangePage={(number) => setCurrentPage(number)} />
-    </div>
-  );
+	return (
+		<div className='container'>
+			<div className='content__top'>
+				<Categories value={categoryId} onChangeCategory={onChangeCategory} />
+				<Sort />
+			</div>
+			<h2 className='content__title'>Все пиццы</h2>
+			<div className='content__items'>{isLoading ? skeletons : dishes}</div>
+			<Pagination onChangePage={(number) => setCurrentPage(number)} />
+		</div>
+	);
 };
 
 export default Home;
