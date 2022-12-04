6f23f5be8f556879f5cfe0dedf1ae46429bf2044
Anton Kulbitskiy <pa1nkller8879@gmail.com>
2022-12-04 03:28:22 +0300

    pagination via redux toolkit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { setCategoryId } from '../redux/slices/filterSlice';
+import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaSkeleton from '../components/Skeleton';
@@ -12,15 +12,18 @@ import { SearchContext } from '../App';
 
 const Home = () => {
 	const dispatch = useDispatch();
-	const { categoryId, sort } = useSelector((state) => state.filter);
+	const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
 
 	const { searchValue } = React.useContext(SearchContext);
 	const [items, setItems] = React.useState([]);
 	const [isLoading, setIsLoading] = React.useState(true);
-	const [currentPage, setCurrentPage] = React.useState(1);
 
 	const onChangeCategory = (id) => {
 		dispatch(setCategoryId(id));
+	};
+
+	const onChangePage = (number) => {
+		dispatch(setCurrentPage(number));
 	};
 
 	React.useEffect(() => {
@@ -55,7 +58,7 @@ const Home = () => {
 			</div>
 			<h2 className='content__title'>Все пиццы</h2>
 			<div className='content__items'>{isLoading ? skeletons : dishes}</div>
-			<Pagination onChangePage={(number) => setCurrentPage(number)} />
+			<Pagination currentPage={currentPage} onChangePage={onChangePage} />
 		</div>
 	);
 };
--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -1,26 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  categoryId: 0,
-  sort: {
-    name: "популярности",
-    sortProperty: "rating",
-  },
+	categoryId: 0,
+	currentPage: 1,
+	sort: {
+		name: 'популярности',
+		sortProperty: 'rating',
+	},
 };
 
 const filterSlice = createSlice({
-  name: "filters",
-  initialState,
-  reducers: {
-    setCategoryId(state, action) {
-      state.categoryId = action.payload;
-    },
-    setSort(state, action) {
-      state.sort = action.payload;
-    },
-  },
+	name: 'filters',
+	initialState,
+	reducers: {
+		setCategoryId(state, action) {
+			state.categoryId = action.payload;
+		},
+		setSort(state, action) {
+			state.sort = action.payload;
+		},
+		setCurrentPage(state, action) {
+			state.currentPage = action.payload;
+		},
+	},
 });
 
-export const { setCategoryId, setSort } = filterSlice.actions;
+export const { setCategoryId, setSort, setCurrentPage } = filterSlice.actions;
 
 export default filterSlice.reducer;
