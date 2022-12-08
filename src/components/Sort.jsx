71e7adb61ca5a3973d479d3ca16bfb9dacc47626
Anton Kulbitskiy <pa1nkller8879@gmail.com>
2022-12-08 14:11:21 +0300

    hide/show popup sort

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -14,6 +14,7 @@ export const list = [
 function Sort() {
 	const dispatch = useDispatch();
 	const sort = useSelector((state) => state.filter.sort);
+	const sortRef = React.useRef();
 
 	const [open, setOpen] = React.useState(false);
 
@@ -22,8 +23,22 @@ function Sort() {
 		setOpen(false);
 	};
 
+	React.useEffect(() => {
+		const handleClickOutside = (event) => {
+			if (!event.path.includes(sortRef.current)) {
+				setOpen(false);
+			}
+		};
+
+		document.body.addEventListener('click', handleClickOutside);
+
+		return () => {
+			document.body.removeEventListener('click', handleClickOutside);
+		};
+	}, []);
+
 	return (
-		<div className='sort'>
+		<div ref={sortRef} className='sort'>
 			<div className='sort__label'>
 				<svg
 					width='10'
