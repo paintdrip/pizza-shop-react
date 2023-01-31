83e67c802f72b7fde92df8b1c26461508664eb38
Anton Kulbitskiy <pa1nkller8879@gmail.com>
2023-01-31 19:49:20 +0300

    fix negative counter

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from 'clsx';
 import { useDispatch } from 'react-redux';
 import { addProduct, CartItem, minusProduct, removeProduct } from '../redux/slices/cartSlice';
 
@@ -53,9 +54,12 @@ const CartItemBlock: React.FunctionComponent<CartItemProps> = ({
         </p>
       </div>
       <div className="cart__item-count">
-        <div
+        <button
+          disabled={count === 1}
           onClick={onClickMinus}
-          className="button button--outline button--circle cart__item-count-minus">
+          className={clsx('button button--outline button--circle cart__item-count-minus', {
+            'cart__item-count-minus--disabled': count === 1,
+          })}>
           <svg
             width="10"
             height="10"
@@ -69,7 +73,7 @@ const CartItemBlock: React.FunctionComponent<CartItemProps> = ({
               d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
               fill="#EB5A1E"></path>
           </svg>
-        </div>
+        </button>
         <b>{count}</b>
         <div
           onClick={onClickPlus}
