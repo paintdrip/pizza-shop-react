c421b62be379ee4051845c483bbf3558f3b743a2
Anton Kulbitskiy <pa1nkller8879@gmail.com>
2023-02-11 16:58:15 +0300

    webpack build optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import './scss/app.scss';
 
 import Home from './pages/Home';
-import FullPagePizza from './pages/FullPagePizza';
-import NotFound from './pages/NotFound';
-import Cart from './pages/Cart';
 import MainLayout from './layouts/MainLayout';
+
+const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */ './pages/Cart'));
+const FullPagePizza = React.lazy(
+  () => import(/* webpackChunkName: "FullPagePizza" */ './pages/FullPagePizza'),
+);
+const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'));
 
 // Основной файл проекта, хранящий данные передаваемые в root
 function App() {
-	return (
-		<Routes>
-			<Route path='/' element={<MainLayout />}>
-				<Route path='' element={<Home />} />
-				<Route path='cart' element={<Cart />} />
-				<Route path='pizza/:id' element={<FullPagePizza />} />
-				<Route path='*' element={<NotFound />} />
-			</Route>
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route
+          path="cart"
+          element={
+            <Suspense fallback={<div>Загрузка корзины...</div>}>
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path="pizza/:id"
+          element={
+            <Suspense fallback={<div>Загрузка страницы товара...</div>}>
+              <FullPagePizza />
+            </Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<div>Загрузка...</div>}>
+              <NotFound />
+            </Suspense>
+          }
+        />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
--- src/pages/FullPagePizza.tsx
+++ src/pages/FullPagePizza.tsx
@@ -1,50 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 
 const FullPagePizza: React.FunctionComponent = () => {
-	const [pizza, setPizza] = React.useState<{
-		imageUrl: string;
-		title: string;
-		price: number;
-		description: string;
-	}>();
+  const [pizza, setPizza] = React.useState<{
+    imageUrl: string;
+    title: string;
+    price: number;
+    description: string;
+  }>();
 
-	const { id } = useParams();
-	const navigate = useNavigate();
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-	React.useEffect(() => {
-		async function fetchPizza() {
-			try {
-				const { data } = await axios.get(
-					'https://635fe9e43e8f65f283bed842.mockapi.io/pizzas/' + id,
-				);
-				setPizza(data);
-			} catch (error) {
-				alert('Ошибка при получении данных о пицце');
-				navigate('/');
-			}
-		}
+  React.useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get(
+          'https://635fe9e43e8f65f283bed842.mockapi.io/pizzas/' + id,
+        );
+        setPizza(data);
+      } catch (error) {
+        alert('Ошибка при получении данных о пицце');
+        navigate('/');
+      }
+    }
 
-		fetchPizza();
-	}, []);
+    fetchPizza();
+  }, []);
 
-	if (!pizza) {
-		return <>Загрузка...</>;
-	}
+  if (!pizza) {
+    return <>Загрузка...</>;
+  }
 
-	return (
-		<div className='container container--pizz'>
-			<div>
-				<img src={pizza.imageUrl} />
-			</div>
-			<div>
-				<h2>{pizza.title}</h2>
-				<h4>{pizza.price} ₽</h4>
-				<h4>{pizza.description}</h4>
-			</div>
-		</div>
-	);
+  return (
+    <div className="container container--pizz">
+      <div>
+        <img src={pizza.imageUrl} />
+      </div>
+      <div>
+        <h2>{pizza.title}</h2>
+        <h4>{pizza.price} ₽</h4>
+        <h4>{pizza.description}</h4>
+        <Link to="/">
+          <button className="button button--outline button--add">
+            <span>Назад</span>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default FullPagePizza;
